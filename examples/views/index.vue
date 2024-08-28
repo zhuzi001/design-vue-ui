46d3f18d1138a0436e0ee1d0b6a0f5b9cb118504
黄小梅 <huangxiaomei@cecinvestment.com>
2024-08-28 16:31:13 +0800

    feat: demo

--- examples/views/index.vue
+++ examples/views/index.vue
@@ -1,123 +1,46 @@
 <template>
   <div style="padding: 48px">
-    <div>
+    <!-- <div>
       <br />
       <button @click="init">点击显示加载中、、、1秒自动关闭</button>
-    </div>
+    </div> -->
 
-    <!-- <d-table
-      :columns="columns"
-      :data-source="data"
-      :pag="pag"
-      @changeSize="changeSize"
-      @setColumns="setColumns"
+    <a-card
+      hoverable
+      v-for="(item, index) in items"
+      :key="index"
     >
-     <a slot="name" slot-scope="{text}">{{ text }}</a>
-    <span slot="customTitle"  slot-scope="{text}">{{text}}<a-icon type="smile-o" /> Name</span>
-      <span slot="action" >
-       添加
-        </span>
-    </d-table> -->
-
+      <component :is="item.component" :data="item.data" />
+      <a-card-meta :title="item.title">
+        <template slot="description"> {{ item.description }} </template>
+      </a-card-meta>
+    </a-card>
   </div>
 </template>
 <script>
+import cascader from './cascader.vue'
+import input from './input.vue'
 export default {
   data () {
-    const columns = [
-      {
-        dataIndex: 'index',
-        key: 'index',
-        // slots: { title: 'customTitle' },
-        scopedSlots: { customRender: 'c-index' }
-      },
-      {
-        dataIndex: 'name',
-        key: 'name',
-        slots: { title: 'customTitle' },
-        scopedSlots: { customRender: 'name' }
-      },
-      {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age'
-      },
-      {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-        scopedSlots: { customRender: 'c-tip' }
-      },
-      {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        scopedSlots: { customRender: 'c-badge' }
-      },
-      {
-        title: 'Tags2',
-        key: 'tags2',
-        dataIndex: 'tags2',
-        config: {},
-        scopedSlots: {
-          customRender: 'c-badge',
-          status: {
-            1: 'success',
-            0: 'error',
-            default: 'warning'
-          },
-          txt: {
-            1: '成功了',
-            0: '失败了',
-            default: '不知道'
-          }
-        }
-      },
-      {
-        title: 'Action',
-        key: 'action',
-        scopedSlots: { customRender: 'action' }
-      }
-    ]
-
-    const data = []
-    for (let index = 0; index < 22; index++) {
-      const element = {
-        key: index,
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-        tags: (index % 2).toString(),
-        tags2: index % 3
-      }
-      data.push(element)
-    }
     return {
-      columns,
-      data,
-      pag: {
-        page: 1,
-        page_size: 10,
-        total: 22
-      }
+      items: [
+        {
+          component: cascader,
+          title: 'cascader 级联',
+          description: 'cascader 级联'
+        },
+        {
+          component: input,
+          title: 'cascader 级联',
+          description: 'cascader 级联'
+        }
+      ]
     }
   },
   methods: {
     init () {
       this.$xmLoading.show()
       this.hide()
-    },
-    hide () {
-      setTimeout(() => {
-        this.$xmLoading.hide()
-      }, 1000)
-    },
-    setColumns (value) {
-      this.columns = value
-    },
-    changeSize (page, pageSize) {
-      this.pag.page = page
-      this.pag.page_size = pageSize
     }
   }
 }

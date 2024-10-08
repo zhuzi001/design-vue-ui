810c8d203fc73a8d19caf14f7a1c2ad60217a610
黄紫枫 <873511650@qq.com>
2024-10-08 16:09:55 +0800

    docs: 文档叙述搜搜过滤功能

---
+++ _vuepressDocs/docs/.vuepress/components/design-vue-ui/cascader/search.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <d-cascader
+        :options="options"
+        placeholder="请选择"
+        showSearch
+        v-model="form.selectValue"
+      ></d-cascader>
+    </div>
+  </template>
+  <script>
+  import baseMixin from "./baseMixin";
+  export default {
+    mixins: [baseMixin],
+    data() {
+      return {
+        form: {
+          selectValue: ["320000"],
+        },
+      };
+    },
+    created() {},
+    methods: {},
+  };
+  </script>
+  
